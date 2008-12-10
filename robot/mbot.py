39064904996f16bed1622a4acb8a674254552c0d
David Valdez <me@gnuget.org>
2008-12-09 21:29:45 -0600

    in this moment mbot is only based in PyGtalkRobot.py  (we don't use that class anymore)

--- robot/mbot.py
+++ robot/mbot.py
@@ -25,6 +25,17 @@ import xmpp
 import urllib
 import re
 import inspect
+import time
+
+from plugins.log import Log
+from plugins.tinyUrl import  tinyUrl
+from plugins.awbox import awbox
+from plugins.mbot_md5 import mbot_md5
+from plugins.unmd5 import unmd5
+from plugins.translate import translate
+from plugins.help import help
+
+
 
 """A simple jabber/xmpp bot framework
 
@@ -45,10 +56,18 @@ class GtalkRobot:
     ########################################################################################################################
     conn = None
     show = "available"
-    status = "PyGtalkRobot"
+    status = "Mbot the robot!"
     commands = None
-    command_prefix = 'command_'
+    command_prefix = 'command_' 
     GO_TO_NEXT_COMMAND = 'go_to_next'
+    patterns  = {
+                    'tinyurl'           : '(^tinyurl)\s+(.*)',
+                    'md5'               : '(^md5)\s+(.*)',
+                    'unmd5'             : '(^unmd5)\s+(.*)',
+                    'translate'         : '(^translate)\s+([a-zA-Z0-9\-]+)\s+(.*)',
+                    'c_translate'       : '(^translate)\s+(.*)',
+                    'awbox'             : '(^awbox)\s+(.+)'
+                }
     ########################################################################################################################
     
     #Pattern Tips:
@@ -60,15 +79,15 @@ class GtalkRobot:
     # X or VERBOSE <=> (?x)         Ignores whitespace outside character sets
     
     #This method is the default action for all pattern in lowest priviledge
-    def command_999_default(self, user, message, args):
-        """.*?(?s)(?m)"""
-        self.replyMessage(user, message)
+    #def command_999_default(self, user, message, args):
+    #    """.*?(?s)(?m)"""
+    #    self.replyMessage(user, message)
 
     ########################################################################################################################
     #These following methods can be only used after bot has been successfully started
 
     #show : xa,away---away   dnd---busy   available--online
-    def setState(self, show, status_text):
+    def set_state(self, show, status_text):
         if show:
             show = show.lower()
         if show == "online" or show == "on" or show == "available":
@@ -118,39 +137,44 @@ class GtalkRobot:
         self.getRoster().Authorize(jid)
     
     ########################################################################################################################
+    #deprecated
     def initCommands(self):
         if self.commands:
             self.commands.clear()
         else:
             self.commands = list()
-        for (name, value) in inspect.getmembers(self):
-            if inspect.ismethod(value) and name.startswith(self.command_prefix):
-                self.commands.append((value.__doc__, value))
-        #print self.commands
+        
+        for (function,expresion) in self.patterns.iteritems():
+            for (name, value) in inspect.getmembers(self):
+                if inspect.ismethod(value) and name == self.command_prefix + function:
+                    self.commands.append({'expresion':expresion,'value':value,'plugin':function})
+    #print self.commands
 
-    def controller(self, conn, message):
+
+    def controller(self,conn,message):
         text = message.getBody()
         user = message.getFrom()
+
         if text:
-            text = text.encode('utf-8', 'ignore')
-            if not self.commands:
-                self.initCommands()
-            for (pattern, bounded_method) in self.commands:
-                match_obj = re.match(pattern, text)
-                if(match_obj):
+            text = text.encode('utf-8','ignore') #change the encoding
+
+            for element  in  self.commands:
+                pattern = element['expresion']  #regular expresion
+                function = element['value'] #function callable
+                plugin =  element['plugin'] #plugin name
+                match = re.match(pattern,text)
+                      
+                if(match):
+                    args = match.groups()
                     try:
-                        return_value = bounded_method(user, text, match_obj.groups())
-                        if return_value == self.GO_TO_NEXT_COMMAND:
-                            pass
-                        else:
-                            break
+                        return_value =  function(user,text,match.groups())
+                        break
                     except:
                         print_info(sys.exc_info())
-                        self.replyMessage(user, traceback.format_exc())
+                        self.replyMessage(user,traceback.format_exc())
 
+    #Accepting All request for all users (anyone can add this bot as  a friend)
     def presenceHandler(self, conn, presence):
-        #print presence
-        #print_info(presence)
         if presence:
             print "-"*100
             print presence.getFrom(), ",", presence.getFrom().getResource(), ",", presence.getType(), ",", presence.getStatus(), ",", presence.getShow()
@@ -178,10 +202,13 @@ class GtalkRobot:
         self.server_host = server_host
         self.server_port = server_port
 
+        #charge all plugins
+        self.initCommands()
+
     def start(self, gmail_account, password):
         jid=xmpp.JID(gmail_account)
         user, server, password = jid.getNode(), jid.getDomain(), password
-        
+
         self.conn=xmpp.Client(server, debug=self.debug)
         #talk.google.com
         conres=self.conn.connect( server=(self.server_host, self.server_port) )
@@ -203,7 +230,7 @@ class GtalkRobot:
         
         self.conn.sendInitPresence()
         
-        self.setState(self.show, self.status)
+        self.set_state(self.show, self.status)
         
         print "Bot started."
         self.GoOn()
@@ -211,8 +238,127 @@ class GtalkRobot:
     ########################################################################################################################
 
 
+
+
+    ############################# BOT FUNCTIONS ###########################
+    #"command_" is the command prefix, "001" is the priviledge num, "setState" is the method name.
+    #This method is used to change the state and status text of the bot.
+    def command_001_setState(self, user, message, args):
+        #the __doc__ of the function is the Regular Expression of this command, if matched, this command method will be called. 
+        #The parameter "args" is a list, which will hold the matched string in parenthesis of Regular Expression.
+        '''(available|online|on|busy|dnd|away|idle|out|off|xa)( +(.*))?$(?i)'''
+        show = args[0]
+        status = args[1]
+        jid = user.getStripped()
+
+        # Verify if the user is the Administrator of this bot
+        if jid == 'ldmiao@gmail.com':
+            print jid, " ---> ",bot.getResources(jid), bot.getShow(jid), bot.getStatus(jid)
+            self.setState(show, status)
+            self.replyMessage(user, "State settings changed！")
+
+    
+
+    #this method is for get a tinyUrl from some site
+    def command_tinyurl(self,user,message,args):
+        
+        if(args[1] == ""):
+            self.replyMessage(user,"Necesitas darme una dirección Valida") 
+        else:
+            url =  args[1]
+
+            tiny = tinyUrl()
+            tiny.set_url(url)
+            url = tiny.main()
+            
+            self.replyMessage(user,url) 
+
+    def command_md5(self,user,message,args):
+        #return the md5 hash form some url
+        if(args[1] == ""):
+            self.replyMessage(user,"Vamos no te voy a generar un md5 hash de una cadena vacia") 
+        else:
+            md = args[1]
+            hash = mbot_md5()
+            hash.set_string(md)
+            
+            hash = hash.main()
+            
+            self.replyMessage(user,hash)
+
+    def command_unmd5(self,user,message,args):
+        #retunrs the original string for some hash
+        if(args[1] == ""):
+            self.replyMessage(user,"Me estas pasando una cadena vacia, por ahí algo anda mal") 
+
+        else:
+            unmd = args[1]
+            word = unmd5()
+            word.set_hash(unmd)
+            
+            word = word.main()
+            self.replyMessage(user,word)
+
+    def command_translate(self,user,message,args):
+        #translate text
+        
+        language_pair =  args[1].split("-")
+        language_pair = language_pair[0]+"|"+language_pair[1]
+        
+        text = args[2]
+        trans = translate()
+        trans.set_paragraph(text)
+        trans.set_lang(language_pair)
+        text = trans.main()
+        self.replyMessage(user,text)
+
+    def command_c_translate(self,user,message,args):
+        args2 = (args[0],'en-es',args[1])
+        self.command_translate(user,message,args2)
+
+    #rado: this methid are under construction
+    #this method is used for get the last feed for rss or atom entry
+    #def command_007_getfeed(self,user,message,args):
+    #'''(^getfeed)\s+(.*)'''
+    #if(args[1] == ""):
+    #        self.replyMessage(user,"El primer  feed de  la cadena vacia es....   la cadena vacia!") 
+    #else:
+    #     feed = args[1]
+         
+    #This method is used to response users.
+
+    def command_help(self,user,message,args):
+        #help 
+        '''(^help)\s+(.*)'''
+        
+        ayuda =  help()
+        ayuda = ayuda.main(args[1])
+        
+        self.replyMessage(user,ayuda)
+
+    def command_awbox(self,user,message,args):
+        #awbox
+
+        if(args[1] == ""):
+            self.replyMessage(user,"Necesitas darme una dirección Valida") 
+        else:
+            url =  args[1]
+
+            tiny = awbox()
+            tiny.set_url(url)
+            url = tiny.main()
+            
+            self.replyMessage(user,url)         
+
+    def command_default(self, user, message, args):
+        #self.replyMessage(user, time.strftime("%Y-%m-%d %a %H:%M:%S", time.gmtime()))
+        cosa = user.getStripped()
+        msg = "Hola, aun soy un pequeño, no entendí lo que quieres que haga intenta con el comando help list"
+        self.replyMessage(user,msg)
+
 ############################################################################################################################
 if __name__ == "__main__":
     bot = GtalkRobot()
-    bot.setState('available', "PyGtalkRobot")
-    bot.start("PyGtalkRobot@gmail.com", "PyGtalkRobotByLdmiao")
+    bot.set_state('available', "PyGtalkRobot")
+    
+    bot.start('mail','password')
